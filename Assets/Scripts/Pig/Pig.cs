fdfba583b4f69ffed8d6f285c28bd5aa570c933c
Korkeshko <korkeshkoalex@gmail.com>
2024-05-15 15:26:03 +0300

    update
    Pig OnDestroy

--- Assets/Scripts/Pig/Pig.cs
+++ Assets/Scripts/Pig/Pig.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Pig : MonoBehaviour
-{
+{ 
     private Rigidbody2D rb;
     [SerializeField]
     private CameraShakes cameraShakes;
@@ -13,24 +11,70 @@ public class Pig : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Borb") 
         {
             cameraShakes.Shake();
-            Destroy(rb.gameObject);
+            Destroy(rb.gameObject); 
+        }
+    }
 
-            GameObject[] pigs = GameObject.FindGameObjectsWithTag("Pig");
-            if (pigs.Length == 1)
-            {
-                print($"You won! Good job!");
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-            else
-            {
-                print($"Headshot!");
-            }       
+    private void OnDestroy()
+    {
+        GameObject[] pigs = GameObject.FindGameObjectsWithTag("Pig");
+        if (pigs.Length == 0)
+        {
+            Debug.LogWarning("You won! Good job!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            Debug.Log("Headshot!");
         }
     }
 }
+
+
+// public class GameManager : MonoBehaviour
+// {
+//     private void Start()
+//     {
+//         Debug.LogWarning("OnHitResult += HitResult");
+//         Pig.OnHitResult += HitResult;
+//     }
+
+//     private void OnDestroy()
+//     {
+//         Pig.OnHitResult -= HitResult;
+//     }
+
+//     private void HitResult(string tag)
+//     {
+//         Debug.LogWarning("HitResult");
+//         if (tag == "Borb")
+//         {
+//             Debug.LogWarning("tabag == 'Borb'");
+//             GameObject[] pigs = GameObject.FindGameObjectsWithTag("Pig");
+//             if (pigs.Length == 1)
+//             {
+//                 Debug.LogWarning("You won! Good job!");
+//                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+//             }
+//             else
+//             {
+//                 Debug.Log("Headshot!");
+//             }
+//         }           
+//         else if (tag == "Block")
+//         {
+//             // TODO переделать
+//             // перенести вызов соприкосновения с pig на borb
+//             // и уже из borb вызвать OnHitResult
+//         }
+//     }
+// }
+
+
+      
